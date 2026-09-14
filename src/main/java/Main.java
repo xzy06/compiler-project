1083ea964d03eaa849d4c12262b7c9c444c3cbbe
xzy06 <12412015@mail.sustech.edu.cn>
2026-09-14 03:36:30 -0400

    initial project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,21 @@
+import generated.Calc1.Calc1Lexer;
+import generated.Calc1.Calc1Parser;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
 public class Main {
     public static void main(String[] args) {
-        AnExampleClass exmaple = new AnExampleClass("qwq");
-        System.out.println(exmaple);
+        // 把要解析的字符串喂给 ANTLR
+        CharStream input = CharStreams.fromString("1 + 2");
+        // 词法分析器
+        Calc1Lexer lexer = new Calc1Lexer(input);
+        // Token 流
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // 语法分析器
+        Calc1Parser parser = new Calc1Parser(tokens);
+        // 从规则 'expression' 开始解析
+        ParseTree tree = parser.expr();
+        // 把树打印出来看看
+        System.out.println(tree.toStringTree(parser));
     }
 }
